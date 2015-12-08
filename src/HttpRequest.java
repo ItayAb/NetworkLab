fc3ba254a980d0e36097ce3bb1e4ec25041229e3
ItayAb <itayabra1@gmail.com>
2015-12-08 18:35:50 +0200

    Bla bla

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class HttpRequest implements Runnable {
 
@@ -13,6 +14,8 @@ public class HttpRequest implements Runnable {
 	private final String NOT_IMPLEMENTED_501 = "501 Not Implemented";
 	private final String BAD_REQUEST_400 = "400 Bad Request";
 	private final String INTERNAL_SERVER_ERROR_500 = "500 Internal Server Error";
+
+	private HashMap<String, String> contentType = new HashMap<>();
 
 	private final static String CRLF = "\r\n";// CLRF “\r” == 0x0d ; [LF] ==
 												// “\n” == 0x0a
@@ -24,6 +27,16 @@ public class HttpRequest implements Runnable {
 	public HttpRequest(Socket socket, ConfigData data) {
 		clientSocket = socket;
 		this.data = data;
+		initDictionary();
+	}
+
+	private void initDictionary() {
+		contentType.put("bmp", "Content-Type: image");
+		contentType.put("gif", "Content-Type: image");
+		contentType.put("png", "Content-Type: image");
+		contentType.put("jpg", "Content-Type: image");
+		contentType.put("html", "Content-Type: text/html");
+		contentType.put("ico", "Content-Type: icon");
 	}
 
 	// Implement the run() method of the Runnable interface.
@@ -53,20 +66,20 @@ public class HttpRequest implements Runnable {
 		if (clientRequestArray[0].startsWith("GET")) { // GET request
 			String pageRequest = extractPageFromRequest(clientRequestArray[0]);
 			File requestedPageFile = new File(data.getRoot() + File.separator + pageRequest);
+
 			if (requestedPageFile.exists()) {
 				// 200_OK
 				// read content
-				String responseMessage = createHttpRespones(HttpResponseCode.OK_200, requestedPageFile);
-				sendMessageToClient(responseMessage, requestedPageFile);
+				responseHandler(HttpResponseCode.OK_200, requestedPageFile);
 			} else {
 				// TODO: return 404
-				sendMessageToClient(NOT_FOUND_404, null);
+				responseHandler(HttpResponseCode.NOT_FOUND_404, null);
 			}
 		} else if (clientRequestArray[0].startsWith("POST")) {
 
 		} else {
 			// TODO: return 501
-			sendMessageToClient(NOT_IMPLEMENTED_501, null);
+			responseHandler(HttpResponseCode.NOT_IMPLEMENTED_501, null);
 		}
 
 		clientSocket.close();
@@ -85,21 +98,38 @@ public class HttpRequest implements Runnable {
 		return pageToReturn;
 	}
 
-	private String createHttpRespones(HttpResponseCode typeOfResponse, File requestedPage) {
+	private void responseHandler(HttpResponseCode typeOfResponse, File requestedPage) {
 		StringBuilder httpResponse = new StringBuilder();
+		byte[] pageContent = null;
 
 		switch (typeOfResponse) {
 		case OK_200:
 			httpResponse.append("HTTP/1.0 200 OK" + CRLF);
 			httpResponse.append("Content-Type: text/html" + CRLF);
 			httpResponse.append("Content-Length: " + requestedPage.getTotalSpace() + CRLF);
-			break;
-		case NOT_FOUND_404:			
-			break;
-		case NOT_IMPLEMENTED_501:
+			httpResponse.append(CRLF);
+			pageContent = getFileContent(requestedPage);
 
 			break;
+		// TODO: get the right size of page size of page
+		case NOT_FOUND_404:
+			//pageContent = createHtmlFormat(NOT_FOUND_404);
+			httpResponse.append("HTTP/1.0 " + NOT_FOUND_404 + CRLF);
+			// httpResponse.append("Content-Type: text/html" + CRLF);
+			// httpResponse.append("Content-Length: " + pageContentStr.length()
+			// + CRLF);
+			//httpResponse.append(CRLF);
+			// httpResponse.append(pageContentStr);
+			break;
+		case NOT_IMPLEMENTED_501:
+			httpResponse.append("HTTP/1.0 " + NOT_IMPLEMENTED_501 + CRLF);
+			httpResponse.append(CRLF);
+			httpResponse.append(createHtmlFormat(NOT_IMPLEMENTED_501 + CRLF));
+			break;
 		case BAD_REQUEST_400:
+			httpResponse.append("HTTP/1.0 " + BAD_REQUEST_400 + CRLF);
+			httpResponse.append(CRLF);
+			httpResponse.append(createHtmlFormat(BAD_REQUEST_400 + CRLF));
 
 			break;
 		case INTERNAL_SERVER_ERROR_500:
@@ -109,34 +139,65 @@ public class HttpRequest implements Runnable {
 			break;
 		}
 
-		httpResponse.append(CRLF);
+		sendMessageToClient(httpResponse.toString(), pageContent);
 
-		return httpResponse.toString();
 	}
 
-	private void sendMessageToClient(String responseMessage, File requestedPage) {
+	private byte[] createHtmlFormat(String text) {
+		String entityBody = "<HTML>" + "<HEAD><TITLE>" + text + "</TITLE></HEAD>" + "<BODY><H1>" + text + "</H1></BODY></HTML>";
+		byte[] entityAsByteArray = new byte[entityBody.length()];
+		for (int i = 0; i < entityBody.length(); i++) {
+			entityAsByteArray[i] = (byte) entityBody.charAt(i);
+		}
+		return entityAsByteArray;
+	}
+
+	private byte[] getFileContent(File fileToGetContent) {
+		FileInputStream fileReader = null;
+		byte[] pageContent = null;
+		try {
+			fileReader = new FileInputStream(fileToGetContent);
+			pageContent = new byte[(int) fileToGetContent.length()];
+			while (fileReader.available() != 0) {
+				fileReader.read(pageContent, 0, (int) fileToGetContent.length());
+			}
+
+			System.out.println("Size is: " + pageContent.length);
+		} catch (Exception e) {
+			// TODO: handle exception
+
+		} finally {
+			if (fileReader != null) {
+				try {
+					fileReader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return pageContent;
+	}
+
+	private void sendMessageToClient(String responseMessage, byte[] pageContent) {
 
 		DataOutputStream writerToClient = null;
-		FileInputStream fileReader = null;
 		try {
 			writerToClient = new DataOutputStream(clientSocket.getOutputStream());
+			// printing response header
+			System.out.println(responseMessage.substring(0, responseMessage.indexOf(CRLF)));
 
-			if (requestedPage != null) {
-				fileReader = new FileInputStream(requestedPage);
-				byte[] pageContent = new byte[(int) requestedPage.length()];
-				// printing response header
-				System.out.println(responseMessage.substring(0, responseMessage.indexOf(CRLF)));
-				writerToClient.writeBytes(responseMessage);
+			writerToClient.writeBytes(responseMessage);
 
-				while (fileReader.available() != 0) {
-					fileReader.read(pageContent, 0, (int) requestedPage.length());
-				}
-
+			if (pageContent != null) {
+				System.out.println("Sending content to Client!");
 				writerToClient.write(pageContent);
-			} else {
-				String entityBody = "<HTML>" + "<HEAD><TITLE>" + responseMessage + "</TITLE></HEAD>" + "<BODY><H1>" + responseMessage + "</H1></BODY></HTML>";
-				writerToClient.writeBytes(entityBody);
-			}
+				
+				for (int i = 0; i < pageContent.length; i++) {
+					System.out.print((char)pageContent[i]);
+				}
+			}			
 
 			writerToClient.flush();
 
@@ -148,9 +209,6 @@ public class HttpRequest implements Runnable {
 			try {
 				if (writerToClient != null) {
 					writerToClient.close();
-				}
-				if (fileReader != null) {
-					fileReader.close();
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
---
+++ src/HttpResponseCode.java
@@ -0,0 +1,8 @@
+
+public enum HttpResponseCode {
+	OK_200,
+	NOT_FOUND_404,
+	NOT_IMPLEMENTED_501,
+	BAD_REQUEST_400,
+	INTERNAL_SERVER_ERROR_500
+}
